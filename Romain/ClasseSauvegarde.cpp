f74f6a6ff89f33d7dba76fa2bbb39b9ea23075db
rgranjon <rgranjon@btslivh.eu>
2020-02-25 16:34:24 +0100

    Modif Dossier

---
+++ Romain/ClasseSauvegarde.cpp
@@ -0,0 +1,10 @@
+#include "Sauvegarde.h"
+
+Sauvegarde::Sauvegarde() {
+}
+
+Sauvegarde::Sauvegarde(const Sauvegarde& orig) {
+}
+
+Sauvegarde::~Sauvegarde() {
+}
---
+++ Romain/ClasseSauvegarde.h
@@ -0,0 +1,14 @@
+#ifndef SAUVEGARDE_H
+#define SAUVEGARDE_H
+
+class Sauvegarde {
+public:
+    Sauvegarde();
+    Sauvegarde(const Sauvegarde& orig);
+    virtual ~Sauvegarde();
+private:
+
+};
+
+#endif /* SAUVEGARDE_H */
+
