b026ece44e731245cad18694632e53df608f24d5
rgranjon <rgranjon@btslivh.eu>
2020-05-06 17:12:33 +0200

    Modif Dossier

--- Romain/Nouveau/defs/Librairie.h
+++ Romain/Nouveau/defs/Librairie.h
@@ -13,14 +13,17 @@ using namespace std;
 #include <iterator>
 #include <list>
 #include <mutex>
+// #include <pugixml> // N'est pas trouvée // XML
 #include <sstream>
 #include <stdexcept>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string>
 #include <string.h>
+//#include <tinyxml.h> // N'est pas trouvée // XML
 #include <thread>
 #include <unistd.h> // erreur sur le include lors de la compilation sous Windows
 #include <vector>
+//#include <xmllite.h> // N'est pas trouvée // XML
 
 #endif /* LIBRAIRIE_H */
--- Romain/Nouveau/src/Sauvegarde.cpp
+++ Romain/Nouveau/src/Sauvegarde.cpp
@@ -10,7 +10,18 @@ Sauvegarde::~Sauvegarde() {
 }
 
 bool Sauvegarde::enregistrerMesure(Mesure mesure) {
-    return 0;
+    /*EXEMPLE*/
+    fstream lecture_file("fichier.xml",ios::in);
+    if(lecture_file.is_open()){
+        string line;
+        while(getline(lecture_file,line)){
+            cout << line << endl;
+        }
+        cout << "Fin de la lecture de données du fichier" << endl;
+        lecture_file.close();
+    } else {
+        cout << "Erreur d'ouverture du fichier" << endl;
+    }
 }
 
 bool Sauvegarde::ajouterAMission(Mesure mesure) {
