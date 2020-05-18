2d37200433800ed9f1a856b13e9e900c535e4b4e
rgranjon <rgranjon@btslivh.eu>
2020-05-18 12:11:22 +0200

    Modif Dossier

--- Romain/Nouveau/src/Sauvegarde.cpp
+++ Romain/Nouveau/src/Sauvegarde.cpp
@@ -9,26 +9,6 @@ Sauvegarde::Sauvegarde(const Sauvegarde& orig) {
 Sauvegarde::~Sauvegarde() {
 }
 
-//CODE INITIAL (INTERNET)
-//bool is_readable(const string & file) {
-//    ifstream fichier(file.c_str());
-//    return !fichier.fail();
-//}
-//
-//bool Sauvegarde::enregistrerMesure(Mesure mesure) {
-//
-//bool Sauvegarde::enregistrerMesure(string textetest) {
-//    if (is_readable("initcube.xml")) { // ERREUR DE COMPILATION
-//        cout << "Fichier existant et lisible" << endl;
-//    } else {
-//        cout << "Fichier inexistant ou non lisible" << endl;
-//    }
-
-bool Sauvegarde::enregistrerMesure() { // ERREUR DE COMPILATION
-    pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("initcube.xml");
-    cout << "Load result: " << result.description() << endl;
-    
 /*  ALGO SAUVEGARDE
  *      Fichier présent ?
  *          si oui : Ouvrir le fichier XML
@@ -36,8 +16,32 @@ bool Sauvegarde::enregistrerMesure() { // ERREUR DE COMPILATION
  *      Ecrire chacune des valeurs dans le fichier XML
  *      Sauvegarder le fichier XML
  *      Fermer le fichier XML
-*/
-    return true; // Pour retourner un booléen
+ */
+
+bool Sauvegarde::enregistrerMesure() { // TEST POUR XML
+    const char* resultat;
+    pugi::xml_document doc;
+    pugi::xml_parse_result result = doc.load_file("initcube.xml");
+    //    cout << "Load result: " << result.description() << endl;
+    resultat = result.description();
+    cout << "Variable resultat : " << resultat << endl;
+    if (resultat == "No error") {
+        cout << "NO ERROR" << endl;
+        return true; // Pour retourner un booléen
+    }
+    if (resultat != "No error") {
+        cout << "ERROR" << endl;
+        return false; // Pour retourner un booléen
+    }
+    if (resultat == 0) {
+        cout << "resultat == 0" << endl;
+        return true; // Pour retourner un booléen
+    }
+    if (resultat == 1) {
+        cout << "resultat == 1" << endl;
+        return true; // Pour retourner un booléen
+    }
+    return true; // Pour avoir un booléen
 }
 
 bool Sauvegarde::ajouterAMission(Mesure mesure) {
