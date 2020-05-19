426e50c5321b0554ed15e090f58d155591e804dd
rgranjon <rgranjon@btslivh.eu>
2020-05-19 17:52:03 +0200

    Modif Dossier

--- Romain/Nouveau/src/Sauvegarde.cpp
+++ Romain/Nouveau/src/Sauvegarde.cpp
@@ -22,25 +22,33 @@ bool Sauvegarde::enregistrerMesure() { // TEST POUR XML
     const char* resultat;
     pugi::xml_document doc;
     pugi::xml_parse_result result = doc.load_file("initcube.xml");
-    //    cout << "Load result: " << result.description() << endl;
+    const char* xmlFile = "initcube.xm"; // POUR TEST ERREUR
+//    const char* xmlFile = "initcube.xml"; // POUR TEST VALIDE
     resultat = result.description();
     cout << "Variable resultat : " << resultat << endl;
-    if (resultat == "No error") {
-        cout << "NO ERROR" << endl;
-        return true; // Pour retourner un booléen
+    if(doc.load_file(xmlFile)) { // Si le fichier existe
+        cout << "Le fichier xml " << xmlFile << " est présent dans le répertoire contenant l'exécutable du projet" << endl;
+        system("sudo nano " + xmlFile); // Erreur de compilation
+    } else {
+        cout << "Le fichier xml " << xmlFile << " n'est pas présent dans le répertoire contenant l'exécutable du projet" << endl;
+        system("touch TESTtestTEST.xml"); // Créer un fichier xml
     }
-    if (resultat != "No error") {
-        cout << "ERROR" << endl;
-        return false; // Pour retourner un booléen
-    }
-    if (resultat == 0) {
-        cout << "resultat == 0" << endl;
-        return true; // Pour retourner un booléen
-    }
-    if (resultat == 1) {
-        cout << "resultat == 1" << endl;
-        return true; // Pour retourner un booléen
-    }
+//    if (resultat == "No error") {
+//        cout << "NO ERROR" << endl;
+//        return true; // Pour retourner un booléen
+//    }
+//    if (resultat != "No error") {
+//        cout << "ERROR" << endl;
+//        return false; // Pour retourner un booléen
+//    }
+//    if (resultat == 0) {
+//        cout << "resultat == 0" << endl;
+//        return true; // Pour retourner un booléen
+//    }
+//    if (resultat == 1) {
+//        cout << "resultat == 1" << endl;
+//        return true; // Pour retourner un booléen
+//    }
     return true; // Pour avoir un booléen
 }
 
