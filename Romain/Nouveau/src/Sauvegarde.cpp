bd853e4f9061e81e252c6af2078579dd5068775b
rgranjon <rgranjon@btslivh.eu>
2020-05-20 18:04:09 +0200

    Modif Dossier

--- Romain/Nouveau/src/Sauvegarde.cpp
+++ Romain/Nouveau/src/Sauvegarde.cpp
@@ -9,47 +9,36 @@ Sauvegarde::Sauvegarde(const Sauvegarde& orig) {
 Sauvegarde::~Sauvegarde() {
 }
 
-/*  ALGO SAUVEGARDE
- *      Fichier présent ?
- *          si oui : Ouvrir le fichier XML
- *          si non : Créer le fichier XML et son arborescence
- *      Ecrire chacune des valeurs dans le fichier XML
- *      Sauvegarder le fichier XML
- *      Fermer le fichier XML
- */
+/////////*SOLUTION 4*/ Utilise les variables 'xmlFile' et 'commande'
+// COMPILATION NON
+// FICHIER TROUVE ?
+// OUVERTURE DU FICHIER ?
+// NOM DE FICHIER AUTOMATIQUE OUI
 
-bool Sauvegarde::enregistrerMesure() { // TEST POUR XML
-    const char* resultat;
+bool Sauvegarde::enregistrerMesure() {
     pugi::xml_document doc;
-    pugi::xml_parse_result result = doc.load_file("initcube.xml");
-    const char* xmlFile = "initcube.xm"; // POUR TEST ERREUR
-//    const char* xmlFile = "initcube.xml"; // POUR TEST VALIDE
-    resultat = result.description();
-    cout << "Variable resultat : " << resultat << endl;
-    if(doc.load_file(xmlFile)) { // Si le fichier existe
-        cout << "Le fichier xml " << xmlFile << " est présent dans le répertoire contenant l'exécutable du projet" << endl;
-        system("sudo nano " + xmlFile); // Erreur de compilation
+    char commande[256];
+    char ouvertureFichier[] = "sudo nano";
+    char xmlFile[] = "initcube.xml"; // POUR TEST VALIDE
+    //    char* xmlFile[] = "nomDuFichierInexistant.xml"; // POUR TEST ERREUR
+    sprintf(commande, "%s %s", ouvertureFichier, xmlFile);
+    if (doc.load_file(xmlFile)) { // Si le fichier existe
+        cout << "Le fichier initcube.xml est présent dans le répertoire contenant l'exécutable du projet" << endl;
+        //OUVRIR LE FICHIER XML
+        system(commande); // ERREUR DE COMPILATION
+        //INSERER LES VALEURS DANS LE FICHIER XML
+        //SAUVEGARDER LE FICHIER XML
+        //FERMER LE FICHIER XML
+        return true;
     } else {
-        cout << "Le fichier xml " << xmlFile << " n'est pas présent dans le répertoire contenant l'exécutable du projet" << endl;
-        system("touch TESTtestTEST.xml"); // Créer un fichier xml
+        cout << "Le fichier initcube.xml n'est pas présent dans le répertoire contenant l'exécutable du projet" << endl;
+        //CREER LE FICHIER XML
+        //OUVRIR LE FICHIER XML
+        //INSERER LES VALEURS DANS LE FICHIER XML
+        //SAUVEGARDER LE FICHIER XML
+        //FERMER LE FICHIER XML
+        return false;
     }
-//    if (resultat == "No error") {
-//        cout << "NO ERROR" << endl;
-//        return true; // Pour retourner un booléen
-//    }
-//    if (resultat != "No error") {
-//        cout << "ERROR" << endl;
-//        return false; // Pour retourner un booléen
-//    }
-//    if (resultat == 0) {
-//        cout << "resultat == 0" << endl;
-//        return true; // Pour retourner un booléen
-//    }
-//    if (resultat == 1) {
-//        cout << "resultat == 1" << endl;
-//        return true; // Pour retourner un booléen
-//    }
-    return true; // Pour avoir un booléen
 }
 
 bool Sauvegarde::ajouterAMission(Mesure mesure) {
