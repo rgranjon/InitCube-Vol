0f7473762332dc36e1661b36a00a5471bf64b135
rgranjon <rgranjon@btslivh.eu>
2020-05-27 17:58:01 +0200

    Modif Dossier

--- Romain/Nouveau/src/Sauvegarde.cpp
+++ Romain/Nouveau/src/Sauvegarde.cpp
@@ -18,43 +18,22 @@ Sauvegarde::~Sauvegarde() {
 bool Sauvegarde::enregistrerMesure() {
     pugi::xml_document doc;
     char commandeCreer[256]; // OK
-    char commandeOuvrir[256]; // OK
-    char commandeInserer[256]; // PAS OK
-    char commandeSauvegarder[256]; // PAS OK
-    char commandeFermer[256]; // PAS OK
-    int nb = 157;
-    ajouterAMission();
-    char ouvertureFichier[] = "sudo nano";
     char xmlFile[] = "initcube.xml"; // POUR TEST VALIDE
-    //    char* xmlFile[] = "nomDuFichierInexistant.xml"; // POUR TEST ERREUR
+    sprintf(commandeCreer, "touch %s", xmlFile);
     if (doc.load_file(xmlFile)) { // Si le fichier existe
         cout << "Le fichier " << xmlFile << " est présent dans le répertoire contenant l'exécutable du projet" << endl;
-        //OUVRIR LE FICHIER XML
-        sprintf(commandeOuvrir, "%s %s", ouvertureFichier, xmlFile);
-        system(commandeOuvrir);
-//        //INSERER LES VALEURS DANS LE FICHIER XML
-//        system(commandeInserer);
-//        //SAUVEGARDER LE FICHIER XML
-//        system(commandeSauvegarder);
-//        //FERMER LE FICHIER XML
-//        system(commandeFermer);
-        return true;
     } else {
         cout << "Le fichier " << xmlFile << " n'est pas présent dans le répertoire contenant l'exécutable du projet" << endl;
         //CREER LE FICHIER XML
-        sprintf(commandeCreer, "touch %s", xmlFile);
         system(commandeCreer);
-        //OUVRIR LE FICHIER XML
-        sprintf(commandeOuvrir, "%s %s", ouvertureFichier, xmlFile);
-        system(commandeOuvrir);
-//        //INSERER LES VALEURS DANS LE FICHIER XML
-//        system(commandeInserer);
-//        //SAUVEGARDER LE FICHIER XML
-//        system(commandeSauvegarder);
-//        //FERMER LE FICHIER XML
-//        system(commandeFermer);
-        return false;
     }
+    //OUVRIR LE FICHIER XML
+    //LIRE ID DANS LE FICHIER XML
+    lireID();
+    //INSERER LES VALEURS DANS LE FICHIER XML
+    //SAUVEGARDER LE FICHIER XML
+    //FERMER LE FICHIER XML
+    return true;
 }
 
 bool Sauvegarde::ajouterAMission() {
@@ -62,16 +41,9 @@ bool Sauvegarde::ajouterAMission() {
 }
 
 unsigned char Sauvegarde::lireID() {
-    int cubeId;
-    char commandeOuvrir[256]; // OK
-    char commandeFermer[256]; // PAS OK
-    char ouvertureFichier[] = "sudo nano";
-    char xmlFile[] = "initcube.xml";
-    //OUVRIR LE FICHIER XML
-    system(commandeOuvrir);
+    unsigned char cubeId;
+    // change node name
     //LIRE L'ID DU CUBE DANS LE FICHIER XML
-//    cubeId = valeur de l'id dans le fichier xml
-    //FERMER LE FICHIER XML
-    system(commandeFermer);
-    return 0;
+    //    cubeId = (unsigned char)valeur de l'id dans le fichier xml
+    return cubeId;
 }
--- Romain/Nouveau/src/Surveillance.cpp
+++ Romain/Nouveau/src/Surveillance.cpp
@@ -12,6 +12,7 @@ Surveillance::~Surveillance() {
 //////////TEST SAUVEGARDE//////////
 
 void Surveillance::surveillerConstantes() { // AJOUTER POUR MAGNETO ET POUR DEMANDE UTILISATEUR
+    // INCLURE LE CODE SUIVANT : "TEST-6 : CONDITIONS + SAUVEGARDE" qui est dans le fichier aide_SURVEILLANCE
     cout << "TEST-5 : SAUVEGARDE" << endl;
     sauvegarde->enregistrerMesure();
     cout << "Reboot du système après la sauvegarde (appel de la fonction reboot->systemeReboot());" << endl;
